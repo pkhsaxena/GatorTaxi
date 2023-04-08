91fb9d1b4f3450e0f089e578082c001906e319cd
Prakhar Saxena <saxenaprakhar1998@gmail.com>
2023-04-08 01:09:13 -0400

    function for testing and ease of use

--- src/com/cop5536/Node.java
+++ src/com/cop5536/Node.java
@@ -2,26 +2,54 @@ package com.cop5536;
 
 public class Node {
 	Ride ride;
+	long key;
 	Color color;
 	Node parent;
 	Node leftChild;
 	Node rightChild;
+	static Node externNode;
 
-	@Override
-	public String toString() {
-		return "Node [ride=" + ride + ", color=" + color + "]";
-	}
-
-	public Node() {
+	public Node(long key) {
+		super();
+		this.key = key;
+		this.color = Color.RED;
 	}
 
 	public Node(Ride ride) {
 		super();
 		this.ride = ride;
+		this.key = ride.rideNumber;
 		this.color = Color.RED;
+	}
+
+	@Override
+	public String toString() {
+		return "Node [rideNumber=" + key + ", color=" + color + "]";
 	}
 
 	public enum Color {
 		RED, BLACK;
+	}
+
+	static Color getColor(Node node) {
+		return node != null ? node.color : Color.BLACK;
+	}
+
+	static void setColor(Node node, Color color) {
+		if (node != null) {
+			node.color = color;
+		}
+	}
+
+	static Node leftChild(Node node) {
+		return node != null ? node.leftChild : null;
+	}
+
+	static Node rightChild(Node node) {
+		return node != null ? node.rightChild : null;
+	}
+
+	static Node parent(Node node) {
+		return node != null ? node.parent : null;
 	}
 }
--- src/com/cop5536/Ride.java
+++ src/com/cop5536/Ride.java
@@ -5,19 +5,22 @@ public class Ride {
 	long rideNumber;
 	int rideCost;
 	int tripDuration;
-	@Override
-	public String toString() {
-		return "Ride [rideNumber=" + rideNumber + "]";
-	}
 
 	int pos;
 
 	public Ride(long rideNumber, int rideCost, int tripDuration) {
-		super();
 		this.rideNumber = rideNumber;
 		this.rideCost = rideCost;
 		this.tripDuration = tripDuration;
 		this.pos = 0;
+	}
+	public Ride(long rideNumber) {
+		this.rideNumber = rideNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "Ride [rideNumber=" + rideNumber + "]";
 	}
 
 }
